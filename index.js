7d65dcc2b1ea97d72a3b4af6a54d033585c36840
mduepont <mglabischewski@stud.hs-bremen.de>
2017-06-02 11:09:45 +0200

    update

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ app.set('view engine', 'ejs');
 app.get('/',(req, res)=>{
     res.render('pages/home',{
         title: 'Home',
-        headline: 'Tiervermittlung',
-        text: 'Wir Menschen tragen eine große Verantwortung den Tieren gegenüber. Wir haben sie gerne um uns herum, um uns nicht einsam zu fühlen und entledigen uns ihrer, sobald wir sie nicht mehr brauchen. Dabei empfinden Tiere wie der Mensch Freude und Schmerz, Glück und Unglück. Auf den folgenden Seiten finden Sie bei uns Hunde, Katzen, Kleintiere, Vögel und sogar Exoten, die darauf warten ein neues Zuhause zu finden.'
+        headline: 'Vermittlung',
+        text: `Wir Menschen tragen eine große Verantwortung den Tieren gegenüber. Wir haben sie gerne um 
+        uns herum, um uns nicht einsam zu fühlen und entledigen uns ihrer, sobald wir sie nicht mehr brauchen. 
+        Dabei empfinden Tiere wie der Mensch Freude und Schmerz, Glück und Unglück. Auf den folgenden Seiten 
+        finden Sie bei uns Hunde, Katzen, Kleintiere, Vögel und sogar Exoten, die darauf warten ein neues 
+        Zuhause zu finden.`
     });
 });
 
@@ -52,14 +56,24 @@ app.get('/exoten',(req, res)=>{
 app.get('/impressum',(req, res)=>{
     res.render('pages/impressum',{
         title: 'Impressum',
-        headline: 'In Progress',
+        headline: 'Impressum',
+        text: ``,
+        headline1: 'Allgemeine Datenschutzerklärung',
+        text1: `Durch die Nutzung unserer Website erklären Sie sich mit der Erhebung,
+                Verarbeitung und Nutzung von Daten gemäß der nachfolgenden Beschreibung einverstanden. 
+                Unsere Website kann grundsätzlich ohne Registrierung besucht werden. Dabei werden Daten wie 
+                beispielsweise aufgerufene Seiten bzw. Namen der abgerufenen Datei, Datum und Uhrzeit zu 
+                statistischen Zwecken auf dem Server gespeichert, ohne dass diese Daten unmittelbar auf 
+                Ihre Person bezogen werden. Personenbezogene Daten, insbesondere Name, Adresse oder 
+                E-Mail-Adresse werden soweit möglich auf freiwilliger Basis erhoben. Ohne Ihre Einwilligung 
+                erfolgt keine Weitergabe der Daten an Dritte.`
     });
 });
 
 app.get('/kontakt',(req, res)=>{
     res.render('pages/kontakt',{
         title: 'Kontakt',
-        headline: 'In Progress',
+        headline: 'Kontaktieren Sie doch uns',
     });
 });
 
