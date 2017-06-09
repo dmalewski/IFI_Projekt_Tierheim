de5a27432f5fdbeb0bda40567d20f3b5268b7f09
hajiba <hbelmass@studi.hs-bremen.de>
2017-06-09 15:03:55 +0200

    new files

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 
+const bodyparser = require('body-parser');
+
 const app = express();
 
-app.use(express.static('./assets'));
+const {persistMail} = require('./lib/persister');
 
+app.use(express.static('./assets'));
+app.use(bodyparser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/',(req, res)=>{
     res.render('pages/home',{
         title: 'Home',
         headline: 'Vermittlung',
-        text: `Wir Menschen tragen eine große Verantwortung den Tieren gegenüber. Wir haben sie gerne um 
-        uns herum, um uns nicht einsam zu fühlen und entledigen uns ihrer, sobald wir sie nicht mehr brauchen. 
-        Dabei empfinden Tiere wie der Mensch Freude und Schmerz, Glück und Unglück. Auf den folgenden Seiten 
-        finden Sie bei uns Hunde, Katzen, Kleintiere, Vögel und sogar Exoten, die darauf warten ein neues 
+        text: `Wir Menschen tragen eine große Verantwortung den Tieren gegenüber. Wir haben sie gerne um
+        uns herum, um uns nicht einsam zu fühlen und entledigen uns ihrer, sobald wir sie nicht mehr brauchen.
+        Dabei empfinden Tiere wie der Mensch Freude und Schmerz, Glück und Unglück. Auf den folgenden Seiten
+        finden Sie bei uns Hunde, Katzen, Kleintiere, Vögel und sogar Exoten, die darauf warten ein neues
         Zuhause zu finden.`
     });
 });
@@ -62,17 +66,17 @@ app.get('/impressum',(req, res)=>{
     res.render('pages/impressum',{
         title: 'Impressum',
         headline: 'Impressum',
-        text: `Sinn und Zweck des Im­pres­sum ist die Kenn­zeich­nung des An­bie­ters. Ver­brau­cher und Wett­be­wer­ber 
-                sol­len durch An­ga­be ei­ner la­dungsfähi­gen Adres­se die Möglich­keit be­kom­men, mit dem Be­trei­ber der 
+        text: `Sinn und Zweck des Im­pres­sum ist die Kenn­zeich­nung des An­bie­ters. Ver­brau­cher und Wett­be­wer­ber
+                sol­len durch An­ga­be ei­ner la­dungsfähi­gen Adres­se die Möglich­keit be­kom­men, mit dem Be­trei­ber der
                 Web­sei­te in Kon­takt zu tre­ten und ggf. recht­li­che Schrit­te ge­gen die­sen ein­zu­lei­ten.`,
         headline1: 'Allgemeine Datenschutzerklärung',
         text1: `Durch die Nutzung unserer Website erklären Sie sich mit der Erhebung,
-                Verarbeitung und Nutzung von Daten gemäß der nachfolgenden Beschreibung einverstanden. 
-                Unsere Website kann grundsätzlich ohne Registrierung besucht werden. Dabei werden Daten wie 
-                beispielsweise aufgerufene Seiten bzw. Namen der abgerufenen Datei, Datum und Uhrzeit zu 
-                statistischen Zwecken auf dem Server gespeichert, ohne dass diese Daten unmittelbar auf 
-                Ihre Person bezogen werden. Personenbezogene Daten, insbesondere Name, Adresse oder 
-                E-Mail-Adresse werden soweit möglich auf freiwilliger Basis erhoben. Ohne Ihre Einwilligung 
+                Verarbeitung und Nutzung von Daten gemäß der nachfolgenden Beschreibung einverstanden.
+                Unsere Website kann grundsätzlich ohne Registrierung besucht werden. Dabei werden Daten wie
+                beispielsweise aufgerufene Seiten bzw. Namen der abgerufenen Datei, Datum und Uhrzeit zu
+                statistischen Zwecken auf dem Server gespeichert, ohne dass diese Daten unmittelbar auf
+                Ihre Person bezogen werden. Personenbezogene Daten, insbesondere Name, Adresse oder
+                E-Mail-Adresse werden soweit möglich auf freiwilliger Basis erhoben. Ohne Ihre Einwilligung
                 erfolgt keine Weitergabe der Daten an Dritte.`
     });
 });
@@ -83,6 +87,28 @@ app.get('/kontakt',(req, res)=>{
         headline: 'Kontakt',
         text: `Haben Sie Fragen, Anregungen, Tipps oder Interesse an unserer Seite, füllen Sie doch einfach
                 das Kontaktformular aus und senden Sie es ab. Wir freuen uns auf Ihre Nachricht! Ihr Team4`
+    });
+});
+
+app.post('/kontakt',(req, res)=>{
+
+  const name =req.body.aName;
+  const vorname =req.body.aFirstname;
+  const telefon =req.body.aPhone;
+  const email =req.body.aemail;
+  const betreff =req.body.aSubject;
+  const nachricht = req.body.aText;
+
+  persistMail(name, name)
+    .then(() => {
+            res.render('pages/danke',{
+                title: `Kontakt, ${name}!`,
+                headline: `dankeschön, ${name}!`,
+                subheadline:'Ihre Nachricht wurde erfolgreich gesendet!',
+            });
+    })
+    .catch((err) => {
+          res.send('hat nicht geklappt');
     });
 });
 
