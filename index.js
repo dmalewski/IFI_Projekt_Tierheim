7087d8e7bb9ab1dbc7fb2c869a0fc0d0aacdb961
Diana Malewski <talim08@hotmail.de>
2017-06-27 14:54:05 +0200

    search v1.1.1

--- index.js
+++ index.js
@@ -10,13 +10,9 @@ const {read, findBreeds} = require("./lib/services/reader");
 
 const {filter} = require("./lib/services/filter");
 
-const {startSchedule} = require("./lib/services/schedule");
-
-
 app.set("view engine","ejs");
 
 app.get("/", async (req, res) => {
-        //startSchedule();
         findBreeds().then((breeds) => {
         res.render("index",{
             title: 'Suche',
@@ -26,41 +22,53 @@ app.get("/", async (req, res) => {
 })
 
 app.get("/search",(req, res) => {
+
+    //Größen
     let sizes = "";
     console.log("size.length: " + req.query.size.length);
     if(req.query.size.length) {
         sizes = req.query.size.split(',');
     }
 
+    //Geschlecht
     let genders = "";
     if(req.query.gender.length) {
         genders = req.query.gender.split(',');
     }
 
+    //Rasse
     let breed ="";
     if(req.query.breed_select.length) {
         breed = req.query.breed_select;
     }
 
-     console.log("age.length: " + req.query.age.length);
+    //Alter
+    console.log("age.length: " + req.query.age.length);
     let ages ="";
     if(req.query.age.length) {
         ages = req.query.age.split(',');
     }
 
-    let traits ="";
-    if(req.query.traits.length) {
-        traits = req.query.traits.split(',');
+    //Kastriert
+    let castrated ="";
+    if(req.query.castrated.length) {
+        castrated = req.query.castrated;
     }
 
-    console.log(sizes);
-    console.log(genders);
-    console.log(breed);
-    console.log(ages);
-    console.log(traits);
+    //Eigenschaften
+    let trait ="";
+    if(req.query.traits.length) {
+        trait = req.query.traits.split(',');
+    }
 
-    filter(sizes,genders,breed,ages,traits).then((results) => {
-        //console.log(results);
+    // console.log(sizes);
+    // console.log(genders);
+    // console.log(breed);
+    console.log("Alter:" +  ages);
+    console.log("Eigenschaften:" + trait);
+
+    filter(sizes,genders,breed,ages,castrated,trait).then((results) => {
+        console.log(results);
         res.render("search",{
             dogs: results,
             message: ""
